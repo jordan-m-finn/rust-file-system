979b42e614a1ca611e12ee15bc9f402ff4486a17
jordan-m-finn <jordan.m.finn17@gmail.com>
2026-01-16 18:52:30 -0800

    Add methods to the disk struct, allowing read and write on blocks

--- file_system/src/disk.rs
+++ file_system/src/disk.rs
@@ -9,3 +9,26 @@ pub struct Disk {
     pub input_buffer: [u8; BLOCK_SIZE], // I[512]
     pub output_buffer: [u8; BLOCK_SIZE], // O[512]
 }
+
+imp Disk {
+    // create a new disk with all bytes initialized to zero
+    pub fn new() -> Self {
+        Disk {
+            data: [[0u8; BLOCK_SIZE]; NUM_BLOCKS],
+            input_buffer: [0u8; BLOCK_SIZE],
+            output_buffer: [0u8; BLOCK_SIZE],
+        }
+    }
+
+    // read block `block_num` from disk into the input buffer
+    pub fn read_block(&mut self, block_num: usize) {
+        // copy the entire block into the input_buffer
+        self.input_buffer.copy_from_slice(&self.data[block_num]);
+    }
+
+    // write the output buffer contents to block `block_num` on disk
+    pub fn write_block(&mut self, block_num: usize) {
+        // copy output_buffer into the disk block
+        self.data[block_num].copy_from_slice(&self.output_buffer);
+    }
+}
